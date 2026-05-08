ed02a11602da8e5b3759c0e061d258d0830fd15c
AdnanFoisal <adnanfoisalarif@gmail.com>
2026-05-08 17:53:37 +0600

    added gpa calculator

---
+++ CGPA Project/GpaCalculator.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CGPA_Project
+{
+    public static class GpaCalculator
+    {
+        public static double CalculateGpa(List<CourseResult> courses)
+        {
+            if (courses == null || courses.Count == 0)
+                return 0.0;
+
+            double totalPoints = 0;
+            double totalCredits;
+
+            foreach(var course in courses)
+            {
+                totalPoints += (course.Credit * course.GradePoint);
+                totalCredits += course.Credits;
+            }
+            return totalCredits == 0 ? 0.0 : totalPoints / totalCredits;
+        }
+    }
+}
