9b728c7670aa60fb2e257bcce8b41b780d758374
AdnanFoisal <adnanfoisalarif@gmail.com>
2026-05-08 17:38:08 +0600

    added sessional corse

---
+++ CGPA Project/SessionalCourse.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace CGPA_Project
+{
+    public class SessionalCourse : CourseResult
+    {
+        public SessionalCourse(string code,double credit,string grade,double gradePoint,string levelTerm) : base(code,credit,grade,gradePoint,levelTerm)
+        { }
+
+        public override string GetCourseInfo()
+        {
+            return $"[Sessional]{Code} | Credits: {Credit:F2} | Grade: {Grade} ({GradePoint:F2}) | Level-Term:{LevelTerm}";
+        }
+    }
+}
