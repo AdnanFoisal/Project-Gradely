0630d355a83382f72df7320e47ef0cbdf1db96c1
AdnanFoisal <adnanfoisalarif@gmail.com>
2026-05-08 17:46:40 +0600

    reggular course file

---
+++ CGPA Project/RegularCourse.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Text;
+
+namespace CGPA_Project
+{
+    public class RegularCourse: CourseResult
+    {
+        public RegularCourse(string code,double credit,string grade,double gradePoint,string levelTerm) : base(code,credit,grade,gradePoint,levelTerm)
+        {
+
+        }
+        public override string GetCourseInfo()
+        {
+            return $"[Theory] {Code} | Credits: {Credit:F2} | Grade: {Grade} ({GradePoint:F2}) | Level-Term: {LevelTerm}";
+        }
+    }
+}
